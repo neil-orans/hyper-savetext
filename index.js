53f9eec2366c99794fceffbb29e7d0717007822b
Neil Orans <neil.orans@gmail.com>
2018-04-04 16:44:02 -0400

    Add basic logging functionality for keybinding

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 'use strict'
 
-const var1 = '';
+const keybinding = 'J';
 
-exports.func1 = config => {
-}
+exports.decorateTerm = (Term, { React, notify }) => {
+  return class extends React.Component {
+    constructor (props, context) {
+      super(props, context);
 
-exports.func2 = defaults => {
-}
+      this._onTerminal = this._onTerminal.bind(this);
+    }
+
+    render () {
+      return React.createElement(Term, Object.assign({}, this.props, {
+        onTerminal: this._onTerminal
+      }));
+    }
+
+    _onTerminal (term) {
+      if (this.props.onTerminal) {
+        this.props.onTerminal(term);
+      }
+
+      this._rootDiv = term.div_;
+
+      this._addKeyboardShortcutHandler(term);
+    }
+
+    _addKeyboardShortcutHandler(term) {
+      const activatingKeyShortcutHandler = [
+        "keydown",
+        function(e) {
+          if ((  (window.process.platform === 'darwin' &&  e.metaKey && !e.ctrlKey)
+              || (window.process.platform !== 'darwin' && !e.metaKey &&  e.ctrlKey))
+              && !e.shiftKey && e.keyCode === keybinding.charCodeAt(0)) {
+            console.log('savetext button pressed!');
+            // this._testFunc();
+          }
+        }.bind(this)
+      ];
+
+      term.uninstallKeyboard();
+      term.keyboard.handlers_ = [...term.keyboard.handlers_, activatingKeyShortcutHandler];
+      term.installKeyboard();
+    }
+
+  // This doesn't work...
+  //   _testFunc() {
+  //       var classnum = prompt("Please enter your class number", "EECS 481");
+  //
+  //       if (classnum == null || classnum == "") {
+  //           txt = "User cancelled the prompt.";
+  //       } else {
+  //           txt = "You are in " + classnum;
+  //       }
+  //       alert(txt);
+  //   }
+  // }
+};
